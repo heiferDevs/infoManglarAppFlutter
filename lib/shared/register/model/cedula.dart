50c29df5b530132706b4714be2a6c9890718bb3b
Ricardo Erazo <erazo.14@hotmail.com>
2021-04-19 17:22:59 -0600

    adding icons and changing JSON to initialize with first letter capitalize

--- lib/shared/register/model/cedula.dart
+++ lib/shared/register/model/cedula.dart
@@ -1,6 +1,4 @@
-
-class Cedula{
-
+class Cedula {
   String cedula;
   String nombre;
   String estadoCivil;
@@ -25,29 +23,26 @@ class Cedula{
   // FROM API
   factory Cedula.fromJson(Map<String, dynamic> json) {
     return Cedula(
-      cedula: json['cedula'],
-      nombre: json['nombre'],
+      cedula: json['Cedula'],
+      nombre: json['Nombre'],
       genero: json['genero'],
       domicilio: json['domicilio'],
       estadoCivil: json['estadoCivil'],
       nacionalidad: json['nacionalidad'],
       calleDomicilio: json['calleDomicilio'],
-      error: json['error'],
+      error: json['Error'],
     );
   }
 
   // TO SAVE
-  Map<String, dynamic> toJson() =>
-  {
-    'cedula': cedula,
-    'error': error,
-    'nombre': nombre,
-    'estadoCivil': estadoCivil,
-    'nacionalidad': nacionalidad,
-    'calleDomicilio': calleDomicilio,
-    'genero': genero,
-    'domicilio': domicilio,
-  };
-
+  Map<String, dynamic> toJson() => {
+        'cedula': cedula,
+        'error': error,
+        'nombre': nombre,
+        'estadoCivil': estadoCivil,
+        'nacionalidad': nacionalidad,
+        'calleDomicilio': calleDomicilio,
+        'genero': genero,
+        'domicilio': domicilio,
+      };
 }
-
